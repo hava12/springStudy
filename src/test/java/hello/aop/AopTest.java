3813019978d1075d2f94cc6c81b45bfd5c79d351
dongyeop <hava12@naver.com>
2022-01-24 00:35:20 +0900

    AOP 프로젝트 테스트

--- src/test/java/hello/aop/AopTest.java
+++ src/test/java/hello/aop/AopTest.java
@@ -7,6 +7,7 @@ import hello.aop.order.aop.AspectV2;
 import hello.aop.order.aop.AspectV3;
 import hello.aop.order.aop.AspectV4Pointcut;
 import hello.aop.order.aop.AspectV5Order;
+import hello.aop.order.aop.AspectV6Advice;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Order;
@@ -24,7 +25,8 @@ import static org.assertj.core.api.Assertions.*;
 // @Import(AspectV2.class)
 // @Import(AspectV3.class)
 // @Import(AspectV4Pointcut.class)
-@Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+// @Import({AspectV5Order.LogAspect.class, AspectV5Order.TxAspect.class})
+@Import(AspectV6Advice.class)
 public class AopTest {
 	@Autowired
 	OrderService orderService;
